5e0e581b2bd5483c827dece3375d255039a2191f
Cole Joko <joko.cole.cj@gmail.com>
2020-08-22 17:15:32 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
