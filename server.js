9e639d5dc0e4593a0cbbfb0725ba075bdc137269
Cole Joko <joko.cole.cj@gmail.com>
2020-08-28 16:13:09 -0600

    pushing to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
